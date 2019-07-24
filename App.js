78cb97b463e7d48f3b75abcbcd20362eb583539c
Miha <miha.kloar@gmail.com>
2019-07-24 21:22:40 +0200

    Data binding and events firing

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: "Jelko"
+        name: "Jelko",
+        job: "Ninja",
+        website: "http://www.thenetninja.co.uk",
+        websiteTag: '<a href="http://www.thenetninja.co.uk">Link #2 </a>',
+        age: 22,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        greet: function(time) {
+            return "Good " + time + " "+this.name;
+        },
+        addYear: function(inc) {
+            this.age += inc;
+        },
+        removeYear: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 });
