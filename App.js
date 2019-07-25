9c664eef3dc041e92bd7e5a577a93322ff8a938b
Miha <miha.kloar@gmail.com>
2019-07-25 18:54:21 +0200

    Two way binding

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 22,
-        x: 0,
-        y: 0
-    },
-    methods: {
-        addYear: function(inc) {
-            this.age += inc;
-        },
-        removeYear: function(dec) {
-            this.age -= dec;
-        },
-        updateXY: function(event) {
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function() {
-            alert('You clicked me');
-        }
-    }
-});
-
-new Vue({
-    el: '#vue-app-keyboard',
-    data: {
         name: '',
         age: '',
     },
