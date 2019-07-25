417b204a1b73f48607ee42dac0c9e27c59dd0cb6
Miha <miha.kloar@gmail.com>
2019-07-25 18:40:44 +0200

    More events

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: "Jelko",
-        job: "Ninja",
-        website: "http://www.thenetninja.co.uk",
-        websiteTag: '<a href="http://www.thenetninja.co.uk">Link #2 </a>',
         age: 22,
         x: 0,
         y: 0
     },
     methods: {
-        greet: function(time) {
-            return "Good " + time + " "+this.name;
-        },
         addYear: function(inc) {
             this.age += inc;
         },
@@ -22,6 +15,25 @@ new Vue({
         updateXY: function(event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        click: function() {
+            alert('You clicked me');
+        }
+    }
+});
+
+new Vue({
+    el: '#vue-app-keyboard',
+    data: {
+        name: '',
+        age: '',
+    },
+    methods: {
+        logName: function() {
+           console.log("Test")
+        },
+        logAge: function() {
+            console.log("Test #2");
         }
     }
 });
