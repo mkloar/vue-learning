52e09f79720c2813a26ca676190992d3ca341410
Miha <miha.kloar@gmail.com>
2019-07-25 19:11:31 +0200

    Computed properties

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: "Jelko"
+        age: 20,
+        a: 0,
+        b: 0
+    },
+    methods: {
+        /*addToA: function() {
+            console.log("AddToA");
+            return this.a + this.age;
+        },
+        addToB: function() {
+            console.log("AddToB");
+            return this.b + this.age; 
+        }*/
+    },
+    computed: {
+        //no more double running both functions for only one buttons
+        addToA: function() {
+            console.log("AddToA");
+            return this.a + this.age;
+        },
+        addToB: function() {
+            console.log("AddToB");
+            return this.b + this.age; 
+        }
     }
 });
