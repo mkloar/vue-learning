9b67ec05fe1fc1b407e9caf2dcc1105114d167a1
GodZilla <miha.kloar@gmail.com>
2019-07-28 11:39:19 +0200

    Looping

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 new Vue({
     el: '#vue-app',
     data: {
-        
+        name: "michael",
+        characters: ['Jelko','Thomas', 'Egan', 'Ana','Browser'],
+        ninjas: [
+            { name: 'Ryu', age: 25},
+            { name: 'YOshi', age: 35},
+            { name: 'Ken', age: 55 }
+        ]
     },
     methods: {
 
